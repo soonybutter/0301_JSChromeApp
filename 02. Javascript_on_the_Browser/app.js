3c56d9f050bff59349193468166f6545b2f37dbd
Yang Dayeon <ydy229@naver.com>
2025-02-21 23:35:18 +0900

    event

--- 02. Javascript_on_the_Browser/app.js
+++ 02. Javascript_on_the_Browser/app.js
@@ -1,3 +1,41 @@
-const title = document.getElementById("title");
+const hellos = document.getElementsByClassName('hello');
 
-title.innerText = "Got you!"; 
+console.log(hellos);
+
+//const omg = document.getElementsByTagName("omg");
+
+//querySelector : class + 태그
+//중복된다면, 첫번째 것만 가져옴!
+const omg = document.querySelector(".omg h1");
+console.log(omg);
+
+const omg1 = document.querySelectorAll(".omg h1");
+console.log(omg);
+
+const omg2 = document.querySelector(".hello");
+console.log(omg2);
+
+
+const title = document.querySelector(".omg");
+
+function handleTitleClick(){
+    console.log("title was clicked!");
+}
+
+
+
+const title1 = document.querySelector("div");
+
+function handleMouseEnter(){
+    title.innerHTML = "Mouse is here !";
+}
+
+
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title1.addEventListener("mouseenter", handleMouseEnter);
+title1.addEventListener("mouseleave", handleMouseLeave);
